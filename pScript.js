11954482ea2dcb056b4e6ac45f3c4f39fa1c9a0e
Andrey-Fedosov <andrefedosov@gmail.commv gitignore .gitignoregit config --global user.email andrefedosov@gmail.com>
2021-11-17 12:13:37 +0300

    добавлена задача про циклы условия

--- pScript.js
+++ pScript.js
@@ -32,13 +32,31 @@ let personalMovieDB = {
 	genres: [],
 	privat: false
 };
- let a = prompt('Один из последних просмотренных фильмов?', ''),
-     b = prompt( 'На сколько оцените его?', ''),
-     c = prompt('Один из последних просмотренных фильмов?', ''),
-     d = prompt( 'На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('произошла ошибка')
+};
+ 
+// В рамках второго задания нужно  автоматизировать вопросы про фильмы при помощи цикла и прописать условия,
+// согласно которым нельзя отменить ответ и не ввести его
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+	  personalMovieDB.movies[a] = b;
+	  console.log('done');
+  } else {
+	  i--;
+	  console.log('error');
+  }
+};
 
 console.log(personalMovieDB);
 
